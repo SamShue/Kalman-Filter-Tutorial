d56c0450825efeed7019c0ac839c9a0153fabd37
Sam Shue <evilrobotfactory@gmail.com>
2023-04-15 14:12:02 -0400

    Added simple x axis position estimator

---
+++ SIMULATION_PositionTracking.m
@@ -0,0 +1,33 @@
+clc;
+clear all;
+close all;
+
+% time between measurements (seconds)
+dt = 1;
+
+% State vector - describes position, velocity
+x = [0; 0];
+% Covariance matrix
+P = eye(2);
+% State transition matrix
+F = [1 dt; 0 1];
+% Control vector - acceleration
+u = [1];
+% Control coefficient matrix - maps control vector into state vector space
+B = [0.5*dt*dt; dt];
+% Process noise matrix
+Q = [0.25 0; 0 0.25];
+
+% Measurement vector - velocity
+z = [1];
+H = [dt 1];
+R = [1];
+
+% Prediction step
+x = F*x + B*u
+P = F*P*F' + Q
+
+% Update step
+K = P*H'*inv(H*P*H' + R)
+x = x + K*(z - H*x)
+P = (eye(2) - K*H)*P*(eye(2) - K*H)' + K*R*K';
